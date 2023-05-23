829a7f2cb20627c5ba55ebc3fcd2b79f3350e387
Parasuramandckap <parasuaptadckap@gmail.com>
2023-05-23 14:34:27 +0530

    reverse a number problem solved

---
+++ reverse a number.php
@@ -0,0 +1,19 @@
+<?php
+
+
+//Write a program in PHP to reverse a number?
+
+$name =readline("Enter the Number:");
+
+//split the given the number like this [0,1] 
+$split_Number = str_split($name);
+
+$output = [];
+//then loop split number and check the i value inside the split number array then i;m store the output array
+for ($i=count($split_Number)-1; $i >=0 ; $i--) { 
+    $output[] = $split_Number[$i];
+
+}
+//implode is type of separater 
+echo implode(',', $output);
+
