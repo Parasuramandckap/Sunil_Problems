c0efc2fbdd56af03b8fb22c14c0e8ad8bccc98f8
Parasuramandckap <parasuaptadckap@gmail.com>
2023-05-27 10:58:49 +0530

    fibnacci problem

--- fibnacci.php
+++ fibnacci.php
@@ -7,12 +7,12 @@ $Number = readline("Enter the Number:");
 function fibnacci($Number){
     $a=0;
     $b=1;
-    $result = $a+$b;
+    
     echo "Fibonacci Series \n";
     
     if(is_numeric($Number)){
         echo $a." ".$b;
-        for($i=2;$i<$Number;$i++){
+        for($i=0;$i<$Number;$i++){
             $result = $a + $b;
             $a = $b;
             $b = $result;
