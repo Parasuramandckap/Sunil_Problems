f8d47f7aabce1a7eceba3a9b5198578ab76dabae
Parasuramandckap <parasuaptadckap@gmail.com>
2023-06-01 10:30:25 +0530

    leap year

---
+++ leapyear.php
@@ -0,0 +1,16 @@
+<?php
+
+$yearInput = readLine("Please enter a Year: ");
+
+function findLeapyear($yearInput){
+    $result = "";
+    if($yearInput % 400 == 0 || $yearInput % 4 == 0 || $yearInput % 100 == 0){
+        $result .= "It's a Leap Year";
+    }
+    else{
+        $result .= "It's not a Leap Year";
+    }
+    return $result;
+}
+
+echo findLeapyear($yearInput);
