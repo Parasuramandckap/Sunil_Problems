bb85fd2deb23c9fcba1c4b1095c788a5f46a460a
Parasuraman Annadurai <113749848+Parasuramandckap@users.noreply.github.com>
2023-06-01 11:30:24 +0530

    Update leapyear.php

--- leapyear.php
+++ leapyear.php
@@ -4,7 +4,10 @@ $yearInput = readLine("Please enter a Year: ");
 
 function findLeapyear($yearInput){
     $result = "";
-    if($yearInput % 400 == 0 || $yearInput % 4 == 0 || $yearInput % 100 != 0){
+    if($yearInput % 4 == 0 && $yearInput % 100 != 0){
+        $result .= "It's a Leap Year";
+    }
+    else if($yearInput % 400 == 0 ){
         $result .= "It's a Leap Year";
     }
     else{
