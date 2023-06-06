c95d3c1d59a70a8f930bceb93673eab491787978
Parasuramandckap <parasuaptadckap@gmail.com>
2023-06-06 12:14:04 +0530

    find array empty or not

---
+++ find_empty_array.php
@@ -0,0 +1,14 @@
+<?php
+
+$array = [];
+
+if(count($array)){
+    echo "**Array is not empty**";
+    echo "\n";
+    print_r($array);
+}
+else{
+    echo "**Array is empty**";
+    echo "\n";
+    print_r($array);
+}
